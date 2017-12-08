9aa56e2170b4230da7820e37b171b150899db158
1453006 <1453006@student.hcmus.edu.vn>
2017-12-08 21:49:33 +0700

    update

--- MultyPlayerVR/Assets/Scripts/MiniGames/HockeyGame.cs
+++ MultyPlayerVR/Assets/Scripts/MiniGames/HockeyGame.cs
@@ -6,13 +6,16 @@ public class HockeyGame : GameCore {
  
     public Transform[] StrikerSpawnPoint;
     public static HockeyGame instance;
+
 #region Objects In Game
     public GameObject strikerPrefab;
     public GameObject ball;
-    #endregion
+    public BoxCollider[] validArea;
+#endregion
 
     private void Awake()
     {
+        currentGame = GameType.Hockey;
         instance = this;
     }
     // Use this for initialization
@@ -29,17 +32,24 @@ public class HockeyGame : GameCore {
     {
         base.OnStartGame();
         Vector3 pos;
-        if(PhotonNetwork.isMasterClient)
-            //if is master room
+        if (PhotonNetwork.isMasterClient)
+        {    //if is master room
             pos = StrikerSpawnPoint[0].position;
+            validArea[1].gameObject.SetActive(false);
+        }
         else
+        {
             pos = StrikerSpawnPoint[1].position;
+            validArea[0].gameObject.SetActive(false);
+        }
 
         GameObject striker = PhotonNetwork.Instantiate(strikerPrefab.name, pos, Quaternion.identity, 0);
         striker.GetPhotonView().RPC("SetParent", PhotonTargets.AllViaServer, this.gameObject.name);
 
         //set active BALL
         ball.SetActive(true);
+
+        
     }
 
 
